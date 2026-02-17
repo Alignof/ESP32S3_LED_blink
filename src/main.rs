3e762aca0213552e24626902c502321cf51c6d55
Alignof <Alignof@outlook.com>
2026-02-17 19:07:25 +0900

    [fix] fix some typos

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,12 @@ fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    // Set GPIO15 (USER LED) as an output, and set its state high initially.
+    // Set GPIO21 (USER LED) as an output, and set its state high initially.
     let mut led = Output::new(peripherals.GPIO21, Level::High, OutputConfig::default());
 
     loop {
         led.toggle();
-        // Wait for half a second
+        // Wait for half second
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
